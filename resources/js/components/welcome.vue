32ba99d0fe1680462205813f7d4c4d0282bb135b
reatweb <reatweb@gmail.com>
2021-02-04 22:12:09 +0300

    first commit

---
+++ resources/js/components/welcome.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="marmara">
+        Hoşgeldin <b> {{ isim }} </b> <br>
+        Yeteneklerin: <b v-for="(skill, i) in skills" :key="i">{{ skill }}, </b> <br>
+        Yaşınız <b> {{ yas }} </b>, 
+        <span v-if="yas<18"> siteye kayıt olabilmek için 18 yaşından büyük olmalısınız.</span>
+        <span v-if="yas>=18"> siteye kayıt olabilirsiniz.</span><br><br>
+        <div class="text-center">
+
+            <button @click="ageInfo" class="btn btn-success">Yaşım Alert</button><br><br>
+
+            <button v-on:click="counter +=1" class="btn btn-primary">Tıkla</button><br>
+            <p><b>{{ counter }}</b></p><br> <!-- Data Counter -->
+
+            <button @click="yasArttir" class="btn btn-danger">Yaşı Arttır</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isim: 'Ömer',
+            yas: 17,
+            skills : ['php', 'laravel', 'vue'],
+            counter: 0,
+        }
+    },
+    methods: {
+        ageInfo(){
+            alert(this.yas)
+        },
+        yasArttir(){
+            this.yas = this.yas+1
+        },
+
+    },
+}
+</script>
+
+<style>
+.marmara {
+    margin-left: 15px;
+    margin-top: 10px;
+}
+</style>
---
+++ resources/views/anasayfa.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>VueJS Ders</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <div id="app">
+        <welcome-component></welcome-component>
+    </div>
+</body>
+    <script src="{{ asset('js/app.js') }}"></script>
+</html>
